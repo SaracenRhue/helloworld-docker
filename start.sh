5388f6721c1e42e568037e4669e81d51b5434709
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2022-09-25 21:12:01 +0200

    upd

--- start.sh
+++ start.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-site="./site"
+site="./site" # set path
 
+# download files if folder is empty
 if [ -z "$(ls -A $site)" ]; then
     cd $site
     wget https://raw.githubusercontent.com/SaracenRhue/helloworld-docker/main/site/index.html
     cd
 fi
 
+chmod 777 $site # set permissions to everyone
 sleep 5
-npm start
+npm start # start server
